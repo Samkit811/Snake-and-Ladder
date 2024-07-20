1ef9a77ac2a6370800d9c4fb6a169a67f1fe638c
Samkit811 <samkitsalot0@gmail.com>
2024-07-20 16:28:21 +0530

    Snake and Ladder Class Added

---
+++ src/Main/Ladder.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.util.HashMap;
+
+public class Ladder {
+    private HashMap<Integer, Integer> ladderPositions;
+
+    public Ladder(){
+        this.ladderPositions = new HashMap<>();
+        this.ladderPositions.put(5, 20);
+        this.ladderPositions.put(15, 35);
+    }
+
+    public boolean playerGetLadder(int position){
+        return this.ladderPositions.containsKey(position);
+    }
+
+    public int playerPositionAfterLadder(int position){
+        return this.ladderPositions.get(position);
+    }
+}
---
+++ src/Main/Snake.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.util.HashMap;
+
+public class Snake {
+    private HashMap<Integer, Integer> snakePositions;
+
+    public Snake(){
+        this.snakePositions = new HashMap<>();
+        this.snakePositions.put(25, 10);
+        this.snakePositions.put(40, 30);
+    }
+
+    public boolean playerGetSnack(int position){
+        return this.snakePositions.containsKey(position);
+    }
+
+    public int playerPositionAfterSnack(int position){
+        return this.snakePositions.get(position);
+    }
+}
